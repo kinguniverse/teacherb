5fd7dd264d82d80b2b1b29ce765f33bcc5fdccc2
kinguniverse <88718381+kinguniverse@users.noreply.github.com>
2022-12-01 00:15:09 +0530

    Add files via upload

---
+++ adsense/j.js
@@ -0,0 +1,694 @@
+$(document).ready(function(){
+    $('#result').hide();
+    $('#example').hide();
+
+    $('#ans').click(function(){
+        $('#result').toggle();
+    })
+    $('#solve').click(function(){
+        $('#example').toggle();
+    })
+     
+})
+
+// second program 
+$(document).ready(function(){
+    $('#result2').hide();
+    $('#example2').hide();
+
+    $('#ans2').click(function(){
+        $('#result2').toggle();
+    })
+    $('#solve2').click(function(){
+        $('#example2').toggle();
+    })
+     
+})
+
+// thirth program 
+$(document).ready(function(){
+    $('#result3').hide();
+    $('#example3').hide();
+
+    $('#ans3').click(function(){
+        $('#result3').toggle();
+    })
+    $('#solve3').click(function(){
+        $('#example3').toggle();
+    })
+     
+})
+
+// fourth program
+$(document).ready(function(){
+    $('#result4').hide();
+    $('#example4').hide();
+
+    $('#ans4').click(function(){
+        $('#result4').toggle();
+    })
+    $('#solve4').click(function(){
+        $('#example4').toggle();
+    })
+     
+})
+
+//  fifth program
+$(document).ready(function(){
+    $('#result5').hide();
+    $('#example5').hide();
+
+    $('#ans5').click(function(){
+        $('#result5').toggle();
+    })
+    $('#solve5').click(function(){
+        $('#example5').toggle();
+    })
+     
+})
+
+// sixth pogram
+$(document).ready(function(){
+    $('#result6').hide();
+    $('#example6').hide();
+
+    $('#ans6').click(function(){
+        $('#result6').toggle();
+    })
+    $('#solve6').click(function(){
+        $('#example6').toggle();
+    })
+     
+})
+
+// seventh program
+
+$(document).ready(function(){
+    $('#result7').hide();
+    $('#example7').hide();
+
+    $('#ans7').click(function(){
+        $('#result7').toggle();
+    })
+    $('#solve7').click(function(){
+        $('#example7').toggle();
+    })
+     
+})
+
+// eighth pogram
+$(document).ready(function(){
+    $('#result8').hide();
+    $('#example8').hide();
+
+    $('#ans8').click(function(){
+        $('#result8').toggle();
+    })
+    $('#solve8').click(function(){
+        $('#example8').toggle();
+    })
+     
+})
+
+// nine program
+$(document).ready(function(){
+    $('#result9').hide();
+    $('#example9').hide();
+
+    $('#ans9').click(function(){
+        $('#result9').toggle();
+    })
+    $('#solve9').click(function(){
+        $('#example9').toggle();
+    })
+     
+})
+
+// tenth program
+$(document).ready(function(){
+    $('#result10').hide();
+    $('#example10').hide();
+
+    $('#ans10').click(function(){
+        $('#result10').toggle();
+    })
+    $('#solve10').click(function(){
+        $('#example10').toggle();
+    })
+     
+})
+
+// eleventh pogram
+$(document).ready(function(){
+    $('#result11').hide();
+    $('#example11').hide();
+
+    $('#ans11').click(function(){
+        $('#result11').toggle();
+    })
+    $('#solve11').click(function(){
+        $('#example11').toggle();
+    })
+     
+})
+
+// twelve program
+$(document).ready(function(){
+    $('#result12').hide();
+    $('#example12').hide();
+
+    $('#ans12').click(function(){
+        $('#result12').toggle();
+    })
+    $('#solve12').click(function(){
+        $('#example12').toggle();
+    })
+     
+})
+
+// thirteenth program
+$(document).ready(function(){
+    $('#result13').hide();
+    $('#example13').hide();
+
+    $('#ans13').click(function(){
+        $('#result13').toggle();
+    })
+    $('#solve13').click(function(){
+        $('#example13').toggle();
+    })
+     
+})
+
+// fourteenth program 
+$(document).ready(function(){
+    $('#result14').hide();
+    $('#example14').hide();
+
+    $('#ans14').click(function(){
+        $('#result14').toggle();
+    })
+    $('#solve14').click(function(){
+        $('#example14').toggle();
+    })
+     
+})
+
+// fifteen program
+$(document).ready(function(){
+    $('#result15').hide();
+    $('#example15').hide();
+
+    $('#ans15').click(function(){
+        $('#result15').toggle();
+    })
+    $('#solve15').click(function(){
+        $('#example15').toggle();
+    })
+     
+})
+
+// sixteen program
+$(document).ready(function(){
+    $('#result16').hide();
+    $('#example16').hide();
+
+    $('#ans16').click(function(){
+        $('#result16').toggle();
+    })
+    $('#solve16').click(function(){
+        $('#example16').toggle();
+    })
+     
+})// seventeen program
+$(document).ready(function(){
+    $('#result17').hide();
+    $('#example17').hide();
+
+    $('#ans17').click(function(){
+        $('#result17').toggle();
+    })
+    $('#solve17').click(function(){
+        $('#example17').toggle();
+    })
+     
+})
+// eighteen program
+$(document).ready(function(){
+    $('#result18').hide();
+    $('#example18').hide();
+
+    $('#ans18').click(function(){
+        $('#result18').toggle();
+    })
+    $('#solve18').click(function(){
+        $('#example18').toggle();
+    })
+     
+})
+// nineteen program
+$(document).ready(function(){
+    $('#result19').hide();
+    $('#example19').hide();
+
+    $('#ans19').click(function(){
+        $('#result19').toggle();
+    })
+    $('#solve19').click(function(){
+        $('#example19').toggle();
+    })
+     
+})
+// twenty program
+$(document).ready(function(){
+    $('#result20').hide();
+    $('#example20').hide();
+
+    $('#ans20').click(function(){
+        $('#result20').toggle();
+    })
+    $('#solve20').click(function(){
+        $('#example20').toggle();
+    })
+     
+})
+
+// 21
+$(document).ready(function(){
+    $('#result21').hide();
+    $('#example20').hide();
+
+    $('#ans21').click(function(){
+        $('#result21').toggle();
+    })
+    $('#solve21').click(function(){
+        $('#example21').toggle();
+    })
+     
+})
+
+// 22
+$(document).ready(function(){
+    $('#result22').hide();
+    $('#example20').hide();
+
+    $('#ans22').click(function(){
+        $('#result22').toggle();
+    })
+    $('#solve22').click(function(){
+        $('#example22').toggle();
+    })
+     
+})
+
+// 23
+$(document).ready(function(){
+    $('#result23').hide();
+    $('#example20').hide();
+
+    $('#ans23').click(function(){
+        $('#result23').toggle();
+    })
+    $('#solve23').click(function(){
+        $('#example23').toggle();
+    })
+     
+})
+
+// 24
+
+$(document).ready(function(){
+    $('#result24').hide();
+    $('#example20').hide();
+
+    $('#ans24').click(function(){
+        $('#result24').toggle();
+    })
+    $('#solve24').click(function(){
+        $('#example24').toggle();
+    })
+     
+})
+
+// 25
+$(document).ready(function(){
+    $('#result25').hide();
+    $('#example20').hide();
+
+    $('#ans25').click(function(){
+        $('#result25').toggle();
+    })
+    $('#solve25').click(function(){
+        $('#example25').toggle();
+    })
+     
+})
+
+// 26
+$(document).ready(function(){
+    $('#result26').hide();
+    $('#example26').hide();
+
+    $('#ans26').click(function(){
+        $('#result26').toggle();
+    })
+    $('#solve26').click(function(){
+        $('#example26').toggle();
+    })
+     
+})
+
+// 27
+$(document).ready(function(){
+    $('#result27').hide();
+    $('#example20').hide();
+
+    $('#ans27').click(function(){
+        $('#result27').toggle();
+    })
+    $('#solve27').click(function(){
+        $('#example27').toggle();
+    })
+     
+})
+
+// 28
+$(document).ready(function(){
+    $('#result28').hide();
+    $('#example20').hide();
+
+    $('#ans28').click(function(){
+        $('#result28').toggle();
+    })
+    $('#solve28').click(function(){
+        $('#example28').toggle();
+    })
+     
+})
+
+// 29
+$(document).ready(function(){
+    $('#result29').hide();
+    $('#example29').hide();
+
+    $('#ans29').click(function(){
+        $('#result29').toggle();
+    })
+    $('#solve29').click(function(){
+        $('#example29').toggle();
+    })
+     
+})
+
+// 30
+$(document).ready(function(){
+    $('#result30').hide();
+    $('#example20').hide();
+
+    $('#ans30').click(function(){
+        $('#result30').toggle();
+    })
+    $('#solve30').click(function(){
+        $('#example30').toggle();
+    })
+     
+})
+
+// 31
+$(document).ready(function(){
+    $('#result31').hide();
+    // $('#example20').hide();
+
+    $('#ans31').click(function(){
+        $('#result31').toggle();
+    })
+    $('#solve31').click(function(){
+        $('#example31').toggle();
+    })
+     
+})
+
+// 32
+$(document).ready(function(){
+    $('#result32').hide();
+    // $('#example20').hide();
+
+    $('#ans32').click(function(){
+        $('#result32').toggle();
+    })
+    $('#solve32').click(function(){
+        $('#example32').toggle();
+    })
+     
+})
+// 33
+$(document).ready(function(){
+    $('#result33').hide();
+    // $('#example33').hide();
+
+    $('#ans33').click(function(){
+        $('#result33').toggle();
+    })
+    $('#solve33').click(function(){
+        $('#example33').toggle();
+    })
+     
+})
+
+// 34
+$(document).ready(function(){
+    $('#result34').hide();
+    // $('#example20').hide();
+
+    $('#ans34').click(function(){
+        $('#result34').toggle();
+    })
+    $('#solve34').click(function(){
+        $('#example34').toggle();
+    })
+     
+})
+
+// 35
+$(document).ready(function(){
+    $('#result35').hide();
+    // $('#example20').hide();
+
+    $('#ans35').click(function(){
+        $('#result35').toggle();
+    })
+    $('#solve35').click(function(){
+        $('#example35').toggle();
+    })
+     
+})
+
+// 36
+$(document).ready(function(){
+    $('#result36').hide();
+    // $('#example20').hide();
+
+    $('#ans36').click(function(){
+        $('#result36').toggle();
+    })
+    $('#solve36').click(function(){
+        $('#example36').toggle();
+    })
+     
+})
+
+// 37
+$(document).ready(function(){
+    $('#result37').hide();
+    // $('#example20').hide();
+
+    $('#ans37').click(function(){
+        $('#result37').toggle();
+    })
+    $('#solve37').click(function(){
+        $('#example37').toggle();
+    })
+     
+})
+
+// 38
+$(document).ready(function(){
+    $('#result38').hide();
+    // $('#example20').hide();
+
+    $('#ans38').click(function(){
+        $('#result38').toggle();
+    })
+    $('#solve38').click(function(){
+        $('#example38').toggle();
+    })
+     
+})
+
+// 39
+$(document).ready(function(){
+    $('#result39').hide();
+    // $('#example20').hide();
+
+    $('#ans39').click(function(){
+        $('#result39').toggle();
+    })
+    $('#solve39').click(function(){
+        $('#example39').toggle();
+    })
+     
+})
+
+// 40
+$(document).ready(function(){
+    $('#result40').hide();
+    // $('#example20').hide();
+
+    $('#ans40').click(function(){
+        $('#result40').toggle();
+    })
+    $('#solve40').click(function(){
+        $('#example40').toggle();
+    })
+     
+})
+
+// 41
+$(document).ready(function(){
+    $('#result41').hide();
+    // $('#example20').hide();
+
+    $('#ans41').click(function(){
+        $('#result41').toggle();
+    })
+    $('#solve41').click(function(){
+        $('#example41').toggle();
+    })
+     
+})
+
+// 42
+$(document).ready(function(){
+    $('#result42').hide();
+    // $('#example20').hide();
+
+    $('#ans42').click(function(){
+        $('#result42').toggle();
+    })
+    $('#solve42').click(function(){
+        $('#example42').toggle();
+    })
+     
+})
+
+// 43
+$(document).ready(function(){
+    $('#result43').hide();
+    // $('#example20').hide();
+
+    $('#ans43').click(function(){
+        $('#result43').toggle();
+    })
+    $('#solve43').click(function(){
+        $('#example43').toggle();
+    })
+     
+})
+
+// 44
+$(document).ready(function(){
+    $('#result44').hide();
+    // $('#example20').hide();
+
+    $('#ans44').click(function(){
+        $('#result44').toggle();
+    })
+    $('#solve44').click(function(){
+        $('#example44').toggle();
+    })
+     
+})
+
+// 45
+$(document).ready(function(){
+    $('#result45').hide();
+    // $('#example45').hide();
+
+    $('#ans45').click(function(){
+        $('#result45').toggle();
+    })
+    $('#solve45').click(function(){
+        $('#example45').toggle();
+    })
+     
+})
+
+// 46
+$(document).ready(function(){
+    $('#result46').hide();
+    // $('#example20').hide();
+
+    $('#ans46').click(function(){
+        $('#result46').toggle();
+    })
+    $('#solve46').click(function(){
+        $('#example46').toggle();
+    })
+     
+})
+
+// 47
+$(document).ready(function(){
+    $('#result47').hide();
+    // $('#example20').hide();
+
+    $('#ans47').click(function(){
+        $('#result47').toggle();
+    })
+    $('#solve47').click(function(){
+        $('#example47').toggle();
+    })
+     
+})
+
+// 48
+$(document).ready(function(){
+    $('#result48').hide();
+    // $('#example48').hide();
+
+    $('#ans48').click(function(){
+        $('#result48').toggle();
+    })
+    $('#solve48').click(function(){
+        $('#example48').toggle();
+    })
+     
+})
+
+// 49
+$(document).ready(function(){
+    $('#result49').hide();
+    $('#example49').hide();
+
+    $('#ans49').click(function(){
+        $('#result49').toggle();
+    })
+    $('#solve49').click(function(){
+        $('#example49').toggle();
+    })
+     
+})
+
+// 50
+$(document).ready(function(){
+    $('#result50').hide();
+    // $('#example20').hide();
+
+    $('#ans50').click(function(){
+        $('#result50').toggle();
+    })
+    $('#solve50').click(function(){
+        $('#example50').toggle();
+    })
+     
+})
